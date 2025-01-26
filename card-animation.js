f3e0876e6c76ffd5b49386ba87a8f638302911c2
Lucy D'Agostino McGowan <lucydagostino@gmail.com>
2025-01-26 12:20:34 -0500

    updte animation

--- card-animation.js
+++ card-animation.js
@@ -3,27 +3,37 @@ let lastClickedTab = 0;
 document.querySelectorAll('.card').forEach((card, index) => {
  const tab = card.querySelector('.tab');
  tab.addEventListener('click', function () {
+  if (lastClickedTab == tab) return;
   const cards = document.querySelectorAll('.card');
-  const isBack = card.classList.contains('card--back');
+  const isBack = card.classList.contains('active');
+  if (isBack) card.classList.remove('active');
 
   cards.forEach((otherCard, otherIndex) => {
-      if (isBack && otherIndex > index) {
-        otherCard.classList.add('active');
-      } else if (!isBack && otherIndex < index) {
-        otherCard.classList.add('active');
-      } else {
+    console.log(otherIndex);
+    const isActive = otherCard.classList.contains('active');
+      if (isBack && otherIndex > index && isActive) {
         otherCard.classList.remove('active');
+        otherCard.style.zIndex =  `calc(1000 - var(--i))`;
+       console.log("im flipping this card up");
+      } else if (!isBack && otherIndex < index && !isActive) {
+        otherCard.classList.add('active');
+       console.log("im flipping this card down")
+      } else {
+        console.log("im doing nothing")
       }
     });
-  console.log(lastClickedTab);
-  if (lastClickedTab !== tab) {
+
   const deck = document.querySelector('.deck');
 
   deck.classList.add('flipping');
   setTimeout(() => {
     deck.classList.remove('flipping');
+    cards.forEach((card, index) => {
+      console.log(index);
+    card.style.zIndex = `calc(100 - var(--i))`;
+  });
    }, 750);
-  }
+
   lastClickedTab = tab;
 
  });
@@ -34,7 +44,7 @@ window.addEventListener('load', function () {
   let delay = 0;
 
   for (let i = 0; i < 3; i++) {
-    const card = cards[i]; // Select individual card
+    const card = cards[i];
     setTimeout(() => {
       card.classList.add('active');
       const deck = document.querySelector('.deck');
